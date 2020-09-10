446e689777ef39f51472c35a6ca31d3926940325
AlMishbah <roihanmisbahulanam140@gmail.com>
2020-09-10 22:38:23 +0700

    Khalas

--- assets/index.js
+++ assets/index.js
@@ -56,8 +56,16 @@ function performCalculation() {
     result =
       parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
   }
-
+  // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+  const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result
+  }
+  putHistory(history);
   calculator.displayNumber = result;
+  renderHistory();
 }
 
 const buttons = document.querySelectorAll(".button");
